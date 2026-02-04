b8de873e7ed65ef7c561e5965c272db44c84b8bf
rakeshshahandco-gif <255803666+rakeshshahandco-gif@users.noreply.github.com>
2026-02-05 01:34:19 +0530

    issues resolved

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { env } from '../config/env';
 
+const currentLocation = window.location.hostname === 'jsk-urja.onrender.com' ? 'https://jsk-urja-backend.onrender.com/api/v1' : 'http://localhost:3000/api/v1'
+
 export const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
+    // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
+    baseURL: currentLocation,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
