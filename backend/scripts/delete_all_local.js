58f621c8a91e42c16cf3850067aaf0f37624072d
rakeshshahandco-gif <255803666+rakeshshahandco-gif@users.noreply.github.com>
2026-03-13 17:59:11 +0530

    example changes

---
+++ backend/scripts/delete_all_local.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
+
+async function execute() {
+    try {
+        console.log('Connecting to: ', process.env.MONGODB_URL ? 'Using MONGODB_URL' : 'Using Fallback');
+        await mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://admin:jskurja@jskurja.wsugxms.mongodb.net/jskurja-dev?retryWrites=true&w=majority');
+        
+        await mongoose.connection.collection('items').deleteMany({});
+        await mongoose.connection.collection('itemgroups').deleteMany({});
+        console.log('Successfully dropped all items and item groups from local Dev DB.');
+        
+        process.exit(0);
+    } catch(err) {
+        console.error(err);
+        process.exit(1);
+    }
+}
+execute();
